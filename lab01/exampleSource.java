ef4a874a78ab13a87b351555d8f6dc527979d08f
Keeran Srikugan <keeran113@gmail.com>
2021-01-21 21:05:54 -0500

    Added new file in lab01 folder

---
+++ lab01/exampleSource.java
@@ -0,0 +1,6 @@
+public class exampleSource {
+    public static void main(String[] args) {
+        System.out.println("Hi! My name is Keeran Srikugan.");
+        System.out.println("I am in second year and I love computer science!");
+    }
+}
