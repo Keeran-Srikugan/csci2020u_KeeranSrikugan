cb0de408e87a1f78822bff4342d0e49ad0b0f90f
Keeran Srikugan <keeran113@gmail.com>
2021-03-02 13:00:35 -0500

    Finished lab 5

--- lab05/src/sample/Controller.java
+++ lab05/src/sample/Controller.java
@@ -8,7 +8,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 public class Controller {
     @FXML private TableView myTable;
 
-    //Below these are the values that will be inputted into their respective columns
+    //Below these are the variables that are associated to the colums int he sample.fxml file
     @FXML private TableColumn<Object, Object> SID;
     @FXML private TableColumn<Object, Object> midterm;
     @FXML private TableColumn<Object, Object> assignments;
@@ -18,7 +18,8 @@ public class Controller {
 
     public void initialize(){
 
-        //The code below associates the data from the class
+        //The code below associates the data from the class to their respective columns
+        //This is the youtube video I used to help with associating the data to the chart(For future reference):https://www.youtube.com/watch?v=4RNhPZJ84P0&t=415s
         SID.setCellValueFactory(new PropertyValueFactory<>("studentID"));
         midterm.setCellValueFactory(new PropertyValueFactory<>("midterm"));
         assignments.setCellValueFactory(new PropertyValueFactory<>("assignments"));
@@ -26,7 +27,7 @@ public class Controller {
         letterGrade.setCellValueFactory(new PropertyValueFactory<>("letterGrade"));
         finalMark.setCellValueFactory(new PropertyValueFactory<>("finalMark"));
 
-        //This is the line of code that sets the items by calling the getAllMarks() function from DataSource.java
+        //This is the line of code that sets the items by calling the getAllMarks() function from DataSource.java file
         myTable.setItems(DataSource.getAllMarks());
     }
 }
--- lab05/src/sample/Main.java
+++ lab05/src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,9 +12,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Pane myPane = (Pane)FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Lab 05 - Solutions");
-        primaryStage.setScene(new Scene(root, 600, 500));
+        Scene myScene = new Scene(myPane);
+        primaryStage.setScene(myScene);
         primaryStage.show();
 
 
